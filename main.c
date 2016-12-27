f313af83e0b6b118150f415d924803d164d38bfa
koi8-r <Burchenya_VP@murmangaz.ru>
2016-12-27 22:10:08 +0300

    1

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 
 int main(int argc, char** argv) {
     printf( "Hello, World!" ) ;
+    AVStream *stream ;
+    AVCodecContext *ctx ;
 
     return 0 ;
 }
